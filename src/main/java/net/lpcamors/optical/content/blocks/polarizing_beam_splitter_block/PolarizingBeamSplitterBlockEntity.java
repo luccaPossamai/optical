9bf520809e618ec7cc2c248e22c9b142de35f977
lpcamors <luccapmarcos@gmail.com>
2024-06-24 14:16:43 -0300

    release 0.1.1

---
+++ src/main/java/net/lpcamors/optical/content/blocks/polarizing_beam_splitter_block/PolarizingBeamSplitterBlockEntity.java
@@ -0,0 +1,19 @@
+package net.lpcamors.optical.content.blocks.polarizing_beam_splitter_block;
+
+import com.simibubi.create.foundation.blockEntity.SmartBlockEntity;
+import com.simibubi.create.foundation.blockEntity.behaviour.BlockEntityBehaviour;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+
+import java.util.List;
+
+public class PolarizingBeamSplitterBlockEntity extends SmartBlockEntity {
+
+    public PolarizingBeamSplitterBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState state) {
+        super(type, pos, state);
+    }
+
+    @Override
+    public void addBehaviours(List<BlockEntityBehaviour> behaviours) {}
+}
